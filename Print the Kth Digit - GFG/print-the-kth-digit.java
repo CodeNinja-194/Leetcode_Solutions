c5fd9a37b4223af9366ab0bd768f4acab4f5d4cb
Adusumilli Sri Ram Prasad <61102759+CodeNinja-194@users.noreply.github.com>
2023-10-04 19:36:34 +0530

    Added solution - LeetHub

---
+++ Print the Kth Digit - GFG/print-the-kth-digit.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-- > 0)
+        {
+            int A = in.nextInt();
+            int B = in.nextInt();
+            int K = in.nextInt();
+            Solution ob = new Solution();
+            System.out.println(ob.kthDigit(A,B,K));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static long kthDigit(int A,int B,int K){
+        // code here
+        double ans=Math.pow(A,B);
+        double rem=0;
+        while(K--!=0)
+        {
+            rem=ans%10;
+            ans/=10;
+        }
+        return (long)rem;
+    }
+}
