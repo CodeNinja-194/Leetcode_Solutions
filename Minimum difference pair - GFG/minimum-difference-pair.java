632777e54b27a5835a23a6a36e08ef38ce0927c7
Adusumilli Sri Ram Prasad <61102759+CodeNinja-194@users.noreply.github.com>
2023-10-12 17:48:44 +0530

    Added solution - LeetHub

---
+++ Minimum difference pair - GFG/minimum-difference-pair.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] nums = new int[n];
+            for(int i = 0; i < n; i++)
+                nums[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.minimum_difference(nums);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int minimum_difference(int[] nums)
+    {
+        // code here
+         Arrays.sort(nums);
+        int k = Integer.MAX_VALUE;
+        int n=nums.length;
+        for(int i = 0 ; i < n -1 ; i++)
+        {
+            k = Math.min(k , nums[i+ 1] - nums[i]);
+        }
+        return k;
+    }
+}
